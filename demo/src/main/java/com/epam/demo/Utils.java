e7e9361c825b4b119da241df468b51590f6c5d2d
Matvey Stetcenko <matveyvs1987@gmail.com>
2023-01-28 17:44:45 +0400

    Second test

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,17 +1,24 @@
 package com.epam.demo;
 
+import com.epam.utils.StringUtils;
 import java.util.ArrayList;
 import java.util.List;
+
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
         List<String> listOfNumbers = new ArrayList<>();
         for (String element : args){
-            if (Integer.parseInt(element) > 0){
+            if (StringUtils.isPositiveNumber(element)){
                 listOfNumbers.add(element);
             }
         }
         return listOfNumbers.size() == args.size();
         //magic happens here
+    }
+
+    public static void main(String[] args) {
+        List<String> testList = List.of ("92", "12", "14", "123");
+        boolean result = isAllPositiveNumbers(testList);
     }
 }
